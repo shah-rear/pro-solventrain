a105c1a12ac7ebaf4bb79f029cec77a717bbde74
shajib1927 <shajibshahrear@gmail.com>
2023-12-29 08:12:40 +0300

    compact obj updated

--- 2705_Compact_Object.js
+++ 2705_Compact_Object.js
@@ -1,6 +1,6 @@
 /*
 
-2705. Compact Objec
+2705. Compact Objec [Leetcode]
 
 Given an object or array obj, return a compact object. A compact object is the same as the original object, except with keys containing falsy values removed. This operation applies to the object and any nested objects. Arrays are considered objects where the indices are keys. A value is considered falsy when Boolean(value) returns false.
 
@@ -81,3 +81,25 @@ function compactObject(input) {
     }
     return obj;
 };
+
+// Solution: 3
+
+var compactObject_03 = function(obj) {
+    if (Array.isArray(obj)) {
+        return obj.filter(Boolean).map(compactObject_03);
+      }
+    
+      if (typeof obj !== 'object' || obj === null) {
+        return obj;
+      }
+    
+      const compactedObj = {};
+      for (const key in obj) {
+        const value = compactObject_03(obj[key]);
+        if (Boolean(value)) {
+          compactedObj[key] = value;
+        }
+      }
+      return compactedObj;
+        
+    };
