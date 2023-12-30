27053cd4230c1c8ebad89d00636f3c08fcaf7386
shajib1927 <shajibshahrear@gmail.com>
2023-12-30 09:22:41 +0300

    3 fnl

--- 2620_Counter.js
+++ 2620_Counter.js
@@ -46,4 +46,61 @@ function createCounter(n) {
     };
   }
 
-  
+  var createCounter = function(n) {
+    return ()=> n++
+};
+
+
+function createCounter(start) {
+    let count = start;
+    return function() {
+      return count++;
+    }
+  }
+
+function* createCounter(start) {
+  let count = start;
+  while (true) {
+    yield count++;
+  }
+}
+
+const counter = createCounter(10);
+console.log(counter.next().value); // 10
+console.log(counter.next().value); // 11
+console.log(counter.next().value); // 12
+
+/*
+
+1. Closures - As I mentioned earlier, closures are an important
+concept in JavaScript, and understanding how they work is essential 
+for solving the counter function problem. Closures are 
+created whenever a function is defined inside another function, and
+the inner function has access to the variables and parameters of the 
+outer function.
+
+2. Higher-order functions - The counter function is an example of a 
+higher-order function, which is a function that takes one or more 
+functions as arguments or returns a function as its result. In this 
+case, the `createCounter` function takes an initial count as a 
+parameter and returns an inner function that increments and returns 
+the count on each call.
+
+3. Function expressions - The implementation of the counter function 
+using a closure involves using a function expression to define the 
+inner function. Function expressions are a way to define a function 
+as a value that can be assigned to a variable or passed as an 
+argument to another function.
+
+4. Arrow functions - Arrow functions are a shorthand syntax for 
+defining functions in JavaScript, and they are commonly used in 
+functional programming. The counter function can also be implemented 
+using an arrow function, as I showed in one of the previous solutions.
+
+5. Arrays and array methods - In the test cases for the counter 
+function problem, the function is called multiple times and the 
+results are returned as an array. To implement this, we can use an 
+array to store the results and an array method like `map()` to call 
+the function multiple times and add the results to the array.
+
+*/
